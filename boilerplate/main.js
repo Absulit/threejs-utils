67860e52aaa657701955076365d8bef9b28406ad
Sebastian Sanabria Diaz <sebastian@absulit.com>
2016-04-25 21:42:59 -0600

    Fixes for WebVR scaling and rotation
    The rotation had a problem that shrinked the image output.

--- boilerplate/main.js
+++ boilerplate/main.js
@@ -2,11 +2,12 @@ var scene,
     camera,
     renderer,
     light,
-    stereoEnabled = true,
+    stereoEnabled = false,
     /* stereoFallbackEnabled has a bug */
     stereoFallbackEnabled = false,
     stereoFallback = false,
-    effect,
+    stereoEffect,
+    vrEffect,
     effectCache,
     noSleep;
 
@@ -14,8 +15,8 @@ var scene,
 var resizeViewport = function(){
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    if(stereoEnabled){
-        effect.setSize(window.innerWidth, window.innerHeight);
+    if(stereoEffect){
+        stereoEffect.setSize(window.innerWidth, window.innerHeight);
     }else{
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
@@ -25,14 +26,14 @@ var toggleStereo = function(){
     stereoEnabled = !stereoEnabled;
     if(stereoEnabled){
         if(effectCache){
-            effect = effectCache
+            stereoEffect = effectCache
         }else{
-            effect = new THREE.StereoEffect(renderer);
-            effect.eyeSeparation = 1;
+            stereoEffect = new THREE.StereoEffect(renderer);
+            stereoEffect.eyeSeparation = 1;
             effectCache = effect;
         }
     }else{
-        effect = null;
+        stereoEffect = null;
     }
     resizeViewport();
 }
@@ -56,17 +57,17 @@ function init() {
 
 
     controls = new THREE.VRControls( camera );
-    effect = new THREE.VREffect( renderer );
+    vrEffect = new THREE.VREffect( renderer );
 
     if(stereoEnabled){
         if ( WEBVR.isAvailable() === true ) {
-            document.body.appendChild( WEBVR.getButton( effect ) );
+            document.body.appendChild( WEBVR.getButton( vrEffect ) );
         }else if(stereoFallbackEnabled){
             console.info('Fallback to StereoEffect');
             stereoFallback = true;
 
-            effect = new THREE.StereoEffect(renderer);
-            effect.eyeSeparation = 1;
+            stereoEffect = new THREE.StereoEffect(renderer);
+            stereoEffect.eyeSeparation = 1;
 
             noSleep = new NoSleep();
         }
@@ -115,9 +116,12 @@ function update() {
     */
 
     if(stereoEnabled){
-        effect.render( scene, camera );
-        if(!stereoFallbackEnabled){
+        if(stereoFallbackEnabled){
+            stereoEffect.render( scene, camera );
+        }else{
+            vrEffect.render( scene, camera );
             controls.update();
+
         }
     }else{
         renderer.render(scene, camera);
